b63771631e24a3adc1913855525b6b55bee0098d
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-11-30 21:33:33 +0300

    It works!

--- src/main/scala/org/github/vsuharnikov/wavesexchange/MainApp.scala
+++ src/main/scala/org/github/vsuharnikov/wavesexchange/MainApp.scala
@@ -8,7 +8,7 @@ import org.github.vsuharnikov.wavesexchange.logic._
 import org.github.vsuharnikov.wavesexchange.source.Csv
 import zio.console._
 import zio.nio.file.{Files, Path}
-import zio.stm.TQueue
+import zio.stm.{STM, TQueue, TRef}
 import zio.{App, Task, ZIO}
 
 import scala.collection.immutable.Queue
@@ -36,35 +36,56 @@ object MainApp extends App {
       }
       // todo stream
       orders <- Files.readAllLines(args.outputDir / "orders.txt").map { xs =>
-        xs.map(Csv.order).collect {
-          case Right(x) => x
+        xs.map(Csv.order)
+          .collect {
+            case Right(x) => x
+          }
+      }
+      portfolios <- STM.atomically(TRef.make(initialClientsPortfolio))
+      currObs <- STM.atomically(TRef.make(Map.empty[AssetPair, OrderBook]))
+      _ <- {
+        ZIO.foreach(orders) { order =>
+          (for {
+            x <- portfolios.get
+            _ <- validate(order, x(order.client)) match {
+              case Left(value) => STM.unit
+              case Right(value) =>
+                for {
+                  o <- currObs.get
+                  (updatedAllPort, updatedOb) <- STM.succeed {
+                    val tmp = o.getOrElse(order.pair, OrderBook.empty)
+                    append(tmp, order, x)
+                  }
+                  _ <- portfolios.set(updatedAllPort)
+                  _ <- currObs.set(o.updated(order.pair, updatedOb))
+                  //            _ <- {
+                  //              val tmp = o.getOrElse(order.pair, OrderBook.empty)
+                  //              currObs.set(o.updated(order.pair, tmp.copy(asks = tmp.asks.appendOrder(order))))
+                  //            }
+                } yield ()
+            }
+          } yield ()).commit
         }
       }
-      process <- ()
-      _ <- ZIO.foreach(orders)()
-      _ <- {
-        val (updatedClientsPortfolio, finalObs, invalidOrders) = mainLoop(initialClientsPortfolio, orders)
+      _ <- portfolios.get.zip(currObs.get).commit.flatMap {
+        case (updatedClientsPortfolio, finalObs) =>
+          //       val (updatedClientsPortfolio, finalObs, invalidOrders) = mainLoop(initialClientsPortfolio, orders)
 
-        val obPortfolio = Group[ClientsPortfolio].inverse(Monoid.combineAll(finalObs.values.map(_.clientsPortfolio)))
-        val finalClientsPortfolio = updatedClientsPortfolio |+| obPortfolio
-        val assetsAfter = Monoid.combineAll(finalClientsPortfolio.values)
+          val obPortfolio = Group[ClientsPortfolio].inverse(Monoid.combineAll(finalObs.values.map(_.clientsPortfolio)))
+          val finalClientsPortfolio = updatedClientsPortfolio |+| obPortfolio
+          val assetsAfter = Monoid.combineAll(finalClientsPortfolio.values)
 
-        putStrLn(
-          show"""Assets before:
-              |${countAssets(initialClientsPortfolio, OrderBook.empty)}
-              |Assets after:
-              |$assetsAfter
-              |Clients portfolio before:
-              |$initialClientsPortfolio
-              |Clients portfolio after:
-              |$finalClientsPortfolio""".stripMargin
-        ) *>
-          putStrLn(s"Final order books:\n${finalObs.mkString("\n")}") *>
-          putStrLn(s"Invalid orders:\n${invalidOrders
-            .map { x =>
-              s"[${x.index}] ${x.order}: ${x.reason}"
-            }
-            .mkString("\n")}")
+          putStrLn(
+            show"""Assets before:
+                  |${countAssets(initialClientsPortfolio, OrderBook.empty)}
+                  |Assets after:
+                  |$assetsAfter
+                  |Clients portfolio before:
+                  |$initialClientsPortfolio
+                  |Clients portfolio after:
+                  |$finalClientsPortfolio""".stripMargin
+          ) *>
+            putStrLn(s"Final order books:\n${finalObs.mkString("\n")}")
       }
     } yield Unit
 
