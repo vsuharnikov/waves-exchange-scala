ec673bcfb189c9b4c30201f1a398f3b58544470f
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2019-12-01 10:42:19 +0300

    Updated dependencies

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,4 @@
-addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.0")
+addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
+
+//resolvers += Resolver.sonatypeRepo("releases")
+//addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.1" cross CrossVersion.full)
--- src/main/scala/org/github/vsuharnikov/wavesexchange/MainApp.scala
+++ src/main/scala/org/github/vsuharnikov/wavesexchange/MainApp.scala
@@ -8,26 +8,19 @@ import org.github.vsuharnikov.wavesexchange.logic._
 import org.github.vsuharnikov.wavesexchange.source.Csv
 import zio.console._
 import zio.nio.file.{Files, Path}
-import zio.stm.{STM, TQueue, TRef}
+import zio.stm.{STM, TMap, TRef}
 import zio.{App, Task, ZIO}
 
-import scala.collection.immutable.Queue
 import scala.reflect.ClassTag
 
 object MainApp extends App {
-  override def run(args: List[String]) =
-    extractArguments(args)
-      .flatMap(logic)
-      .tapError { e =>
-        putStrLn(e.getMessage)
-      }
-      .fold(_ => 1, _ => 0)
+  override def run(args: List[String]) = extractArguments(args).flatMap(logic).tapError(e => putStrLn(e.getMessage)).fold(_ => 1, _ => 0)
 
   private def extractArguments(args: List[String]): Task[Arguments] =
     if (args.isEmpty) Task.fail(new IllegalArgumentException("Specify a directory with clients.txt and orders.txt"))
     else Task(Arguments(Path(args.head)))
 
-  type OrderBookQueues = Map[AssetPair, TQueue[Order]]
+  type OrderBookQueues = TMap[AssetPair, Order]
 
   private def logic(args: Arguments) =
     for {
@@ -36,77 +29,50 @@ object MainApp extends App {
       }
       // todo stream
       orders <- Files.readAllLines(args.outputDir / "orders.txt").map { xs =>
-        xs.map(Csv.order)
-          .collect {
-            case Right(x) => x
-          }
+        xs.map(Csv.order).collect { case Right(x) => x }
       }
-      portfolios <- STM.atomically(TRef.make(initialClientsPortfolio))
-      currObs <- STM.atomically(TRef.make(Map.empty[AssetPair, OrderBook]))
-      _ <- {
-        ZIO.foreachPar(orders) { order =>
-          (for {
-            x <- portfolios.get
-            _ <- validate(order, x(order.client)) match {
-              case Left(value) => STM.unit
-              case Right(value) =>
-                for {
-                  o <- currObs.get
-                  (updatedAllPort, updatedOb) <- STM.succeed {
-                    val tmp = o.getOrElse(order.pair, OrderBook.empty)
-                    append(tmp, order, x)
-                  }
-                  _ <- portfolios.set(updatedAllPort)
-                  _ <- currObs.set(o.updated(order.pair, updatedOb))
-                  //            _ <- {
-                  //              val tmp = o.getOrElse(order.pair, OrderBook.empty)
-                  //              currObs.set(o.updated(order.pair, tmp.copy(asks = tmp.asks.appendOrder(order))))
-                  //            }
-                } yield ()
+      portfoliosRef <- STM.atomically(TRef.make(initialClientsPortfolio))
+      currObsRef <- STM.atomically(TRef.make(Map.empty[AssetPair, OrderBook]))
+      _ <- ZIO.foreach(orders) { order =>
+        portfoliosRef.get.flatMap { portfolios =>
+          validate(order, portfolios(order.client)).fold(
+            _ => STM.unit,
+            _ =>
+              currObsRef.get.flatMap { currObs =>
+                val (updatedAllPort, updatedOb) = append(currObs.getOrElse(order.pair, OrderBook.empty), order, portfolios)
+                portfoliosRef.set(updatedAllPort) *> currObsRef.set(currObs.updated(order.pair, updatedOb))
             }
-          } yield ()).commit
-        }
+          )
+        }.commit
       }
-      _ <- portfolios.get.zip(currObs.get).commit.flatMap {
-        case (updatedClientsPortfolio, finalObs) =>
-          //       val (updatedClientsPortfolio, finalObs, invalidOrders) = mainLoop(initialClientsPortfolio, orders)
-
-          val obPortfolio = Group[ClientsPortfolio].inverse(Monoid.combineAll(finalObs.values.map(_.clientsPortfolio)))
-          val finalClientsPortfolio = updatedClientsPortfolio |+| obPortfolio
-          val assetsAfter = Monoid.combineAll(finalClientsPortfolio.values)
-
-          putStrLn(
-            show"""Assets before:
-                  |${countAssets(initialClientsPortfolio, OrderBook.empty)}
-                  |Assets after:
-                  |$assetsAfter
-                  |Clients portfolio before:
-                  |$initialClientsPortfolio
-                  |Clients portfolio after:
-                  |$finalClientsPortfolio""".stripMargin
-          ) *>
-            putStrLn(s"Final order books:\n${finalObs.mkString("\n")}")
-      }
-    } yield Unit
+      _ <- portfoliosRef.get.zip(currObsRef.get).commit.flatMap(Function.tupled(printResults(initialClientsPortfolio, _, _)))
+    } yield ()
 
   private case class Arguments(outputDir: Path)
 
   // https://github.com/estatico/scala-newtype/issues/10
   private val assets = List('$', 'A', 'B', 'C', 'D').map(AssetId(_)).toArray(implicitly[ClassTag[Char]].asInstanceOf[ClassTag[AssetId]])
 
-  private def mainLoop(clients: ClientsPortfolio, orders: Iterable[Order]): (ClientsPortfolio, Map[AssetPair, OrderBook], Queue[InvalidOrder]) =
-    orders.zipWithIndex.foldLeft((clients, Map.empty[AssetPair, OrderBook], Queue.empty[InvalidOrder])) {
-      case (curr @ (origAllPort, currObs, invalidOrders), (order, i)) =>
-        validate(order, origAllPort(order.client)) match {
-          case Left(e) => curr.copy(_3 = invalidOrders.enqueue(InvalidOrder(i, order, e)))
-          case Right(_) =>
-            val ob = currObs.getOrElse(order.pair, OrderBook.empty)
-            val (updatedAllPort, updatedOb) = append(ob, order, origAllPort)
-            (updatedAllPort, currObs.updated(order.pair, updatedOb), invalidOrders)
-        }
-    }
-
   private case class InvalidOrder(index: Int, order: Order, reason: String)
+
+  private def printResults(initialClientsPortfolio: ClientsPortfolio,
+                           updatedClientsPortfolio: ClientsPortfolio,
+                           updatedObs: Map[AssetPair, OrderBook]) = {
+    val obPortfolio = Group[ClientsPortfolio].inverse(Monoid.combineAll(updatedObs.values.map(_.clientsPortfolio)))
+    val finalClientsPortfolio = updatedClientsPortfolio |+| obPortfolio
+    val assetsAfter = Monoid.combineAll(finalClientsPortfolio.values)
+
+    putStrLn(
+      show"""Assets before:
+              |${countAssets(initialClientsPortfolio, OrderBook.empty)}
+              |Assets after:
+              |$assetsAfter
+              |Clients portfolio before:
+              |$initialClientsPortfolio
+              |Clients portfolio after:
+              |$finalClientsPortfolio""".stripMargin
+    ) *> putStrLn(s"Final order books:\n${updatedObs.mkString("\n")}")
+  }
 }
 
 class ParseException(message: String) extends RuntimeException(message)
